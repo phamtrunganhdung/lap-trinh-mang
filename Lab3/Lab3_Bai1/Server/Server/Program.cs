1e76df6a84aec3e6afbe3cf16b2d31ce88e59d77
Phạm Trung Anh Dũng <dungpham4320@gmail.com>
2022-04-23 20:43:01 +0700

    add lab3

---
+++ Lab3/Lab3_Bai1/Server/Server/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5000);
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            serverSocket.Bind(serverEndPoint);
+
+            Console.WriteLine("Dang cho Client ket noi");
+
+            EndPoint clientEndpoint = new IPEndPoint(IPAddress.Any, 0);
+
+            byte[] buffer = new byte[1024];
+            int receivedByte;
+
+            receivedByte = serverSocket.ReceiveFrom(buffer, ref clientEndpoint);
+
+            string dataStr = Encoding.ASCII.GetString(buffer, 0, receivedByte);
+
+            Console.WriteLine("Du lieu cua Client: " + dataStr);
+
+            Console.ReadLine();
+
+            serverSocket.Close();
+        }
+    }
+}
