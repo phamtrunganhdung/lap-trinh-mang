497b262da30f8a6801452f21d4c45dc7af24fc4e
Phạm Trung Anh Dũng <dungpham4320@gmail.com>
2022-04-23 20:43:11 +0700

    add lab4

---
+++ Lab4/Client/Client/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee em1 = new Employee();
+            string dataSubmit = "";
+
+            Console.WriteLine("Nhap ID Nhan Vien");
+            em1.EmployeeID = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap Ho");
+            em1.LastName = Console.ReadLine();
+            Console.WriteLine("Nhap Ten");
+            em1.FirstName = Console.ReadLine();
+            Console.WriteLine("Nhap So Nam Lam Viec");
+            em1.YearsService = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap Luong Nhan Vien");
+            em1.Salary = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Xac nhan gui thong tin...");
+            Console.WriteLine("Bam phim bat ki de tiep tuc...");
+            Console.WriteLine("Bam 'khong' de thoat");
+            dataSubmit = Console.ReadLine();
+
+
+            if (dataSubmit == "khong")
+            {
+                Console.WriteLine("khong the gui goi tin");
+                return;
+            }
+            else
+            {
+                TcpClient client;
+                try
+                {
+                    client = new TcpClient("127.0.0.1", 9050);
+                }
+                catch (SocketException)
+                {
+                    Console.WriteLine("Khong ket noi duoc voi server");
+                    return;
+                }
+
+                NetworkStream ns = client.GetStream();
+                byte[] data = em1.GetBytes();
+                int size = em1.size;
+                byte[] packsize = new byte[2];
+                Console.WriteLine("Kich thuoc goi tin = {0}", size);
+                packsize = BitConverter.GetBytes(size);
+                ns.Write(packsize, 0, 2);
+                ns.Write(data, 0, size);
+                ns.Flush();
+
+                ns.Close();
+                client.Close();
+            }
+
+            
+        }
+
+
+       
+    }
+}
---
+++ Lab4/Server/Server/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.IO;
+using System.Text;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] data = new byte[1024];
+            TcpListener server = new TcpListener(IPAddress.Any, 9050);
+            server.Start();
+            TcpClient client = server.AcceptTcpClient();
+            NetworkStream ns = client.GetStream();
+
+            byte[] size = new byte[2];
+            int recv = ns.Read(size, 0, 2);
+            int packsize = BitConverter.ToUInt16(size, 0);
+            Console.WriteLine("kich thuoc goi tin = {0}", packsize);
+            recv = ns.Read(data, 0, packsize);
+            Employee em1 = new Employee(data);
+            Console.WriteLine("ID Nhan Vien = {0}", em1.EmployeeID);
+            Console.WriteLine("Ho = {0}", em1.LastName);
+            Console.WriteLine("Ten = {0}", em1.FirstName);
+            Console.WriteLine("So Nam Lam Viec = {0}", em1.YearsService);
+            Console.WriteLine("Luong = {0} VND\n", em1.Salary);
+            string[] nhanVien = { em1.EmployeeID.ToString(), em1.LastName, em1.FirstName, em1.YearsService.ToString(), em1.Salary.ToString() };
+            File.WriteAllLines("E:\\TaiLieu\\lap-trinh-mang-dlu\\Lab4\\Server\\Server\\file\\nhanvien.txt", nhanVien);
+            Console.WriteLine("Luu vao file thanh cong");
+
+            ns.Close();
+            client.Close();
+            server.Stop();
+        }
+    }
+}
